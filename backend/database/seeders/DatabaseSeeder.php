9e878a12b21098d2bc19e1f04829346468dc6c3d
hamzagbouri <hamzagbouri2004@gmail.com>
2024-08-11 23:18:45 +0200

    first commit

---
+++ backend/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+
+        \App\Models\User::factory()->create([
+            'name' => 'hamza',
+            'email' => 'hamza@gmail.com',
+            'password' => bcrypt('mamababa'),
+
+        ]);
+    }
+}
